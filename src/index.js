3dd8e7c345edfbf899abd35a93e8e890b5606745
Aleksandr <dentranscom@gmail.com>
2022-02-16 10:16:29 +0200

    feat: result brackets

--- src/index.js
+++ src/index.js
@@ -1,3 +1,14 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
-}
+  let result = [];
+  let condition = null;
+    for (let i = 0; i < str.length; i++) {
+        if (result.length === 0) {
+            result.push(str[i]);
+        } else if (result !=0) {
+            bracketsConfig.forEach(elem => (elem.includes(result[result.length - 1]) && (condition = elem)));
+            str[i] === condition[1] ? result.pop() : result.push(str[i]);
+        }
+    }
+
+    return result.length > 0 ? false : true;
+};
